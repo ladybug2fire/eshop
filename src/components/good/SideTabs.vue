d89dbe8278a5f21a90bd8bf2da2e066ddddc8e4f
浪漫不属意 <1391936847@qq.com>
2019-04-15 23:27:11 +0800

    feat: tab切换

--- src/components/good/SideTabs.vue
+++ src/components/good/SideTabs.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="slide-container">
     <div class="side-tabs">
-        <div :class="['tab', i==5?'active':'']" v-for="i in 15" :key="i"></div>
+        <div :class="['tab', i==activeIndex?'active':'']" v-for="(t,i) in tabs" :key="i" @click="activeIndex = i">{{t}}</div>
     </div>
     <div class="content">
         <good-item></good-item>
@@ -11,10 +11,20 @@
 
 <script>
 import GoodItem from '@/components/good/GoodItem'
-
+import {types} from '@/util/constants'
 export default {
     components:{
        GoodItem 
+    },
+    data() {
+        return {
+            activeIndex: 0,
+        }
+    },
+    computed:{
+        tabs(){
+            return types;
+        }
     }
 }
 </script>
@@ -26,14 +36,16 @@ export default {
     display: flex;
     flex-direction: row;
     .side-tabs{
-        width: 100px;
+        width: 90px;
         background: #f8f8f8;
         height: 100%;
         flex-shrink: 0;
+        text-align: center;
         .tab{
             width: 100%;
             height: 40px;
             padding: 10px;
+            font-size: 10px;
             box-sizing: border-box;
             &.active{
                 background: white;
